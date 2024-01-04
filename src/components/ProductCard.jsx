e657c6fba6d34884368fb3a2fbf5ffa0f5ac262e
“red” <karatasmert255@gmail.com>
2024-01-04 10:33:46 +0300

    Update text color in ProductCard component

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -22,7 +22,7 @@ const ProductCard = ({ item }) => {
       </div>
       <div className="mt-4 flex justify-between">
         <div className="flex-1">
-          <h3 className="text-sm text-gray-700 line-clamp-1">{title}</h3>
+          <h3 className="text-sm text-gray-800 line-clamp-1">{title}</h3>
           <p className="mt-1 text-sm text-gray-500 line-clamp-1">{category}</p>
         </div>
         <p className="text-sm font-medium text-gray-900">{price} $</p>
